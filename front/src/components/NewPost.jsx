d0a135eaaf3ef97309b19210b64ffc4e2cdbddbb
seyoung4503 <seyoung4503@gmail.com>
2025-02-08 00:02:43 +0900

    feat : add Modal feature

--- front/src/components/NewPost.jsx
+++ front/src/components/NewPost.jsx
@@ -1,25 +1,15 @@
-import { useState } from 'react';
 import classes from './NewPost.module.css';
 
-function NewPost() {
-    const [ enteredBody, setEnteredBody ] = useState('');
-
-    function changeBodyHandler(event) {
-        setEnteredBody(event.target.value);
-    }
-
+function NewPost(props) {
     return (
         <form className={classes.form}>
             <p>
                 <label htmlFor="body">Text</label>
-                <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
-            </p>
-            <p>
-                {enteredBody}
+                <textarea id="body" required rows={3} onChange={props.onBodyChange}/>
             </p>
             <p>
                 <label htmlFor="name">Your name</label>
-                <textarea type="text" id="name" required />
+                <textarea type="text" id="name" required onChange={props.onAuthorChange}/>
             </p>
         </form>
     );
--- front/src/components/PostsList.jsx
+++ front/src/components/PostsList.jsx
@@ -1,19 +1,44 @@
+import { useState } from "react";
+
 import Post from "./Post";
-import NewPost from './NewPost';
-import classes from './PostsList.module.css';
-
-
+import NewPost from "./NewPost";
+import Modal from "./Modal";
+import classes from "./PostsList.module.css";
 
 function PostsList(props) {
-    return (
-        <>
-            <NewPost />
-            <ul className={classes.posts}>
-                <Post author="Maximilian" body="React.js is awesome!" />
-                <Post author="Manuel" body="Check out the full course!" />
-            </ul>
-        </>
-    );
-};
+  const [modalIsVisible, setModalIsVisible] = useState(true);
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
+
+  function hideModalHandler() {
+    setModalIsVisible(false);
+  }
+
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  return (
+    <>
+      {modalIsVisible ? (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      ) : null}
+
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Manuel" body="Check out the full course!" />
+      </ul>
+    </>
+  );
+}
 
 export default PostsList;
