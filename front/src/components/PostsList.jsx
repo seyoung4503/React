f558b35e542e460325dc074945746939e9ffb02c
seyoung4503 <seyoung4503@gmail.com>
2025-02-08 00:14:16 +0900

    feat : Header & New Post feature

--- front/src/components/PostsList.jsx
+++ front/src/components/PostsList.jsx
@@ -5,14 +5,9 @@ import NewPost from "./NewPost";
 import Modal from "./Modal";
 import classes from "./PostsList.module.css";
 
-function PostsList(props) {
-  const [modalIsVisible, setModalIsVisible] = useState(true);
+function PostsList({ isPosting, onStopPosting }) {
   const [enteredBody, setEnteredBody] = useState("");
   const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  function hideModalHandler() {
-    setModalIsVisible(false);
-  }
 
   function bodyChangeHandler(event) {
     setEnteredBody(event.target.value);
@@ -24,8 +19,8 @@ function PostsList(props) {
 
   return (
     <>
-      {modalIsVisible ? (
-        <Modal onClose={hideModalHandler}>
+      {isPosting ? (
+        <Modal onClose={onStopPosting}>
           <NewPost
             onBodyChange={bodyChangeHandler}
             onAuthorChange={authorChangeHandler}
